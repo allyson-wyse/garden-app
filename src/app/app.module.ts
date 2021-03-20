b29376e7abc6fc32402f7a60eb1c7906df7b13b5
Allyson Wyse <Allyson.Wyse@53.com>
2021-03-19 22:05:08 -0400

    add basic material table with data from local json file + minimal styling to not look like total junk

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,16 +4,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
 import { FooterComponent } from './footer/footer.component';
+import { CalendarListComponent } from './calendar-list/calendar-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    CalendarListComponent,
     FooterComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
+    MatTableModule,
     MatToolbarModule
   ],
   providers: [],
---
+++ src/app/calendar-list/calendar-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import SeedData from '../../assets/seed-data.json';
+
+@Component({
+  selector: 'app-calendar-list',
+  templateUrl: './calendar-list.component.html',
+  styleUrls: ['./calendar-list.component.css']
+})
+export class CalendarListComponent implements OnInit {
+    displayedColumns: string[] = ['name', 'category', 'recommendation'];
+    tableData: any[];
+
+    constructor() { }
+
+    ngOnInit(): void {
+        this.tableData = SeedData['plants'];
+    }
+
+}
